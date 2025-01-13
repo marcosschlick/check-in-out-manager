3060983226955d45b0f9d04c1ccd94a1cccbe34d
Marcos Schlick <marcosschlick@gmail.com>
2025-01-13 19:20:13 -0300

    chore: adicionar estrutura inicial do controller Usuario

---
+++ src/main/java/com/controlador/presenca/controller/UsuarioController.java
@@ -0,0 +1,11 @@
+package com.controlador.presenca.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+
+public class UsuarioController {
+
+}
