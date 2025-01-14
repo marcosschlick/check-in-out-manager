b586e34184a88f19db411cb3eaf462d822483115
Marcos Schlick <marcosschlick@gmail.com>
2025-01-14 09:46:04 -0300

    feat: adiciona endpoints para listar todos e buscar usuários por ID

--- src/main/java/com/controlador/presenca/controller/UsuarioController.java
+++ src/main/java/com/controlador/presenca/controller/UsuarioController.java
@@ -1,11 +1,29 @@
 package com.controlador.presenca.controller;
 
+import com.controlador.presenca.entity.Usuario;
+import com.controlador.presenca.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
-
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public Iterable<Usuario> getUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Usuario> getUsuarioById(@PathVariable int id) {
+        return usuarioRepository.findById(id);
+    }
 }
+
