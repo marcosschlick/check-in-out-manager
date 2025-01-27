7d704551b6836e4f89b099f7aa6e714ba85d5767
Marcos Schlick <marcosschlick@gmail.com>
2025-01-27 20:27:22 -0300

    feat: adicionar UsuarioDTO para transferência de dados da entidade Usuario

---
+++ src/main/java/com/controlador/presenca/dto/UsuarioDTO.java
@@ -0,0 +1,43 @@
+package com.controlador.presenca.dto;
+
+import com.controlador.presenca.entities.Usuario;
+
+public class UsuarioDTO {
+
+    private Long id;
+    private String nome;
+    private String documento;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(Usuario entity) {
+        this.id = entity.getId();
+        this.nome = entity.getNome();
+        this.documento = entity.getDocumento();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+}
