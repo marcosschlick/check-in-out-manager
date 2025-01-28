461991d396d2275e156f2c871f2a06d9708134ba
Marcos Schlick <marcosschlick@gmail.com>
2025-01-27 21:14:05 -0300

    chore: adicionar import.sql com dados iniciais para usuários e presenças

--- src/main/java/com/controlador/presenca/repositories/PresencaRepository.java
+++ src/main/java/com/controlador/presenca/repositories/PresencaRepository.java
@@ -3,11 +3,10 @@ package com.controlador.presenca.repositories;
 import com.controlador.presenca.entities.Presenca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface PresencaRepository extends JpaRepository<Presenca, Long> {
 
-    Optional<Presenca> findTopByUsuarioIdOrderByIdDesc(Long usuarioId);
+    Presenca findTopByUsuarioIdOrderByIdDesc(Long usuarioId);
 
 }
