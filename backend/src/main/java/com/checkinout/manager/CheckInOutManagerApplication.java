cab67a726f1de3007252dc1984bcc94081341d37
Marcos Schlick <marcosschlick@gmail.com>
2025-07-14 11:28:30 -0300

    chore: mudar .env para a raiz do projeto

--- backend/src/main/java/com/checkinout/manager/CheckInOutManagerApplication.java
+++ backend/src/main/java/com/checkinout/manager/CheckInOutManagerApplication.java
@@ -1,13 +1,17 @@
 package com.checkinout.manager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CheckInOutManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheckInOutManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().directory("../").ignoreIfMissing().load();
 
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(CheckInOutManagerApplication.class, args);
+    }
 }
